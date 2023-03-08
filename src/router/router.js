2e1b0c0ca45e8ac3157078b1f16c7efa5ce0307e
(Messi) Junlin Wu <tallmessiwu@qq.com>
2023-03-08 04:05:33 -0800

    :bricks: 更新了项目结构。并增加了utils文件夹。

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,42 @@
 import {createRouter, createWebHashHistory} from "vue-router";
+import Index from "../views/Index.vue";
+import Home from "../views/Home.vue";
+import Page2 from "../views/Page2.vue";
+import MobileIndex from "../views/mobile-index.vue";
+import MobileHome from "../views/mobile-home.vue";
 
 const routes = [
-
+    {
+        path: "/",
+        name: "Index",
+        component: Index,
+        redirect: "/home",
+        children: [
+            {
+                path: "home",
+                name: "Home",
+                component: Home
+            },
+            {
+                path: "page2",
+                name: "Page2",
+                component: Page2
+            }
+        ]
+    },
+    {
+        path: "/mobile",
+        name: "MobileIndex",
+        component: MobileIndex,
+        redirect: "/mobile/home",
+        children: [
+            {
+                path: "home",
+                name: "MobileHome",
+                component: MobileHome
+            }
+        ]
+    }
 ]
 
 const router = createRouter({
